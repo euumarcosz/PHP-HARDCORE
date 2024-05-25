5eedec47443bb8d248feb8179bad3c59b61c3a21
euumarcosz <marcosdesigner98@gmail.com>
2024-05-25 01:45:17 -0300

    build: :memo:

---
+++ Introdução/O que faz o PHP.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 
+ * O que faz o PHP?
+ * 
+ * 1- O PHP pode ser dividido em duas vertentes:
+ * 
+ * R01- Programação backend: Criação de sites dinâmicos, conexão e interação com banco de dados, geração de gráfico, documentos de XML e PDF; 
+ * 
+ * R02- Scripts de linha de comando: Rodar scripts para que açôes sejam executadas no computador ou remotamente, administração de sistema ou até mesmo CRONs; 
+ * 
+ */
