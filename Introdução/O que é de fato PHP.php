c306b7f3b7d89a0137ae717d109c536d945b88b1
euumarcosz <marcosdesigner98@gmail.com>
2024-05-25 02:10:22 -0300

    build: :memo: Comentário

---
+++ Introdução/O que é de fato PHP.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * 
+ * O que é de fato PHP? 
+ * 
+ * 1- Uma linguagem de programação que foi desenvolvida para deixar o HTML dinâmico; 
+ * 
+ * 2- Linguagem de script e open source; 
+ * 
+ * 3- O foco de PHP é o desenvolvimento web; 
+ * 
+ * 4- Os programas são executados em server side (lado do servidro, backend);
+ * 
+ * 5- PHP significa: Personal Home Page (P) e Hypertext Preprocessor (HP).
+ * 
+ */
