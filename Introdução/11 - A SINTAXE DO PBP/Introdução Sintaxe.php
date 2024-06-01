348600de9ec9d6288d06f7835fa0a94720485e80
euumarcosz <marcosdesigner98@gmail.com>
2024-06-01 17:49:21 -0300

    Leitura de comentario "echo"

---
+++ Introdução/11 - A SINTAXE DO PBP/Introdução Sintaxe.php
@@ -0,0 +1,20 @@
+<?php
+
+echo"
+/**
+ * 
+ * A sintaxe do PHP
+ * 
+ * 1- O PHP vai interpretar um bloco de código em nosso arquirvo apenas se ele estiver entre as tags de PHP;
+ * 
+ * 2- A abertura é: <?php
+ * 
+ * 3- E o fechamento: ?>
+ * 
+ * 4- Coloque ; a cada instrução; 
+ * 
+ * 5- Todo o codigo dentro destas tasg será executado pelo PHP, e após isso será impresso na tela;
+ * 
+ */
+";
+?>
