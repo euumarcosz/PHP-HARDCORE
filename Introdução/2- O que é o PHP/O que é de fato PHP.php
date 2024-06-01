bb03db0080ee78dd36c846d797c701a57bd92b1c
euumarcosz <marcosdesigner98@gmail.com>
2024-06-01 18:04:41 -0300

    Contexto organizacional

--- Introdução/2- O que é o PHP/O que é de fato PHP.php
+++ Introdução/2- O que é o PHP/O que é de fato PHP.php
@@ -1,20 +1,18 @@
 <?php
  
  echo "
-/**
- * 
- * O que é de fato PHP? 
- * 
- * 1- Uma linguagem de programação que foi desenvolvida para deixar o HTML dinâmico; 
- * 
- * 2- Linguagem de script e open source; 
- * 
- * 3- O foco de PHP é o desenvolvimento web; 
- * 
- * 4- Os programas são executados em server side (lado do servidor, backend);
- * 
- * 5- PHP significa: Personal Home Page (P) e Hypertext Preprocessor (HP).
- * 
- */
+
+ O que é de fato PHP? 
+
+ 1- Uma linguagem de programação que foi desenvolvida para deixar o HTML dinâmico; 
+
+ 2- Linguagem de script e open source; 
+
+ 3- O foco de PHP é o desenvolvimento web; 
+
+ 4- Os programas são executados em server side (lado do servidor, backend);
+
+ 5- PHP significa: Personal Home Page (P) e Hypertext Preprocessor (HP).
+
 ";
  ?>
