86e0b6b4e5bb9973a9532f93b38743345652bbb5
euumarcosz <marcosdesigner98@gmail.com>
2024-06-01 18:09:50 -0300

    Contexto organizacional

--- Introdução/6 - Insalando executando o PHP no Linux/Instalação PHP Linux.php
+++ Introdução/6 - Insalando executando o PHP no Linux/Instalação PHP Linux.php
@@ -1,16 +1,14 @@
 <?php 
 
-echo "
-/**
- *
- * Instalação PHP linux
- * 
- * 1- No Linux podemos instalar o PHP de forma independente, porém é interessante fazer a instalação da pilha LAMP;
- * 
- * 2- Linux, Apache, MySQL e PHP;
- * 
- * 3- Com isso podemos criar e executar não só arquivos de PHP mas também projetos web completos;
- * 
- */
-";
+ echo "
+
+ Instalação PHP linux
+ 
+ 1- No Linux podemos instalar o PHP de forma independente, porém é interessante fazer a instalação da pilha LAMP;
+ 
+ 2- Linux, Apache, MySQL e PHP;
+ 
+ 3- Com isso podemos criar e executar não só arquivos de PHP mas também projetos web completos;
+ 
+ ";
 ?>
