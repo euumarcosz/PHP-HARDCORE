2ef57bc3c12f828bdb0ecdaa8a77a1e45334a42a
euumarcosz <marcosdesigner98@gmail.com>
2024-06-01 18:15:34 -0300

    Contexto organizacional

--- Introdução/10- O que é localhost/O que é localhost.php
+++ Introdução/10- O que é localhost/O que é localhost.php
@@ -1,18 +1,16 @@
 <?php
 
-echo "
-/**
- * 
- * O que é localhost?
- * 
- * 1- É a forma de acessar o servidor local da nossa máquina;
- * 
- * 2- Equivale ao IP 127.0.0.1, ou seja, nosso PC;
- * 
- * 3- Assim podemos simular como se o site ou arquivo que estamos acessando é processado em um servidor;
- * 
- * 4- IP significa Internet Protocol;
- * 
- */
+ echo "
+
+ O que é localhost?
+ 
+ 1- É a forma de acessar o servidor local da nossa máquina;
+ 
+ 2- Equivale ao IP 127.0.0.1, ou seja, nosso PC;
+ 
+ 3- Assim podemos simular como se o site ou arquivo que estamos acessando é processado em um servidor;
+ 
+ 4- IP significa Internet Protocol;
+ 
 "
 ?>
