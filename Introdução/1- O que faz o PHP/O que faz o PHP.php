797f70d38f53ca51c0375f3827abcdb2bb05a440
euumarcosz <marcosdesigner98@gmail.com>
2024-06-01 18:03:05 -0300

    Contexto organizacional

--- Introdução/1- O que faz o PHP/O que faz o PHP.php
+++ Introdução/1- O que faz o PHP/O que faz o PHP.php
@@ -1,16 +1,14 @@
 <?php
 
 echo "
-/**
- * 
- * O que faz o PHP?
- * 
- * O PHP pode ser dividido em duas vertentes:
- * 
- * 1- Programação backend: Criação de sites dinâmicos, conexão e interação com banco de dados, geração de gráfico, documentos de XML e PDF; 
- * 
- * 2- Scripts de linha de comando: Rodar scripts para que açôes sejam executadas no computador ou remotamente, administração de sistema ou até mesmo CRONs; 
- * 
- */
-";
+
+ O que faz o PHP?
+ 
+ O PHP pode ser dividido em duas vertentes:
+ 
+ 1- Programação backend: Criação de sites dinâmicos, conexão e interação com banco de dados, geração de gráfico, documentos de XML e PDF; 
+ 
+ 2- Scripts de linha de comando: Rodar scripts para que açôes sejam executadas no computador ou remotamente, administração de sistema ou até mesmo CRONs; 
+
+ ";
 ?>
