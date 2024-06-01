c0aacafc05fd80fa2b257c96d262b4d34da666c5
euumarcosz <marcosdesigner98@gmail.com>
2024-06-01 18:13:21 -0300

    Contexto organizacional

--- Introdução/8- Informativo sobre o XAMPP e MySQL/Informativo sobre o XAMPP e MySQL.php
+++ Introdução/8- Informativo sobre o XAMPP e MySQL/Informativo sobre o XAMPP e MySQL.php
@@ -1,18 +1,16 @@
 <?php
 
-echo "
-/**
- * 
- * Infomativo sobre XAMPP e MySQL
- * 
- * Olá alunos, como vocês estão? 
- * 
- * Espero que estejam bem!
- * 
- * 1- Na próxima aula, vou explicar como baixar e instalar o MySQL atualizado. No entanto, se você já possui o XAMPP instalado, não é necessário acompanhar essa parte específica da aula.
- * 
- * 2- Força nos estudos, galera! Um grande abraço a todos.
- * 
- * /
+ echo "
+
+ Infomativo sobre XAMPP e MySQL
+ 
+ Olá alunos, como vocês estão? 
+ 
+ Espero que estejam bem!
+ 
+ 1- Na próxima aula, vou explicar como baixar e instalar o MySQL atualizado. No entanto, se você já possui o XAMPP instalado, não é necessário acompanhar essa parte específica da aula.
+ 
+ 2- Força nos estudos, galera! Um grande abraço a todos.
+ 
  ";
  ?>
