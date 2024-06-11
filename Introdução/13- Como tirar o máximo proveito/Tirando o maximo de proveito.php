e3095a73d32841abc5c94f979cc9266629802f80
euumarcosz <marcosdesigner98@gmail.com>
2024-06-11 14:50:38 -0300

    feat: :white_check_mark: Contexto organizacional

---
+++ Introdução/13- Como tirar o máximo proveito/Tirando o maximo de proveito.php
@@ -0,0 +1,18 @@
+<?php
+
+ echo"
+
+ Máximo de proveito
+
+ 1- Faça todos os exemplos com o editor aberto e codifique junto;
+
+ 2- Crie seus próprios exemplos com o que foi aprendido na aula;
+
+ 3- Crie tambem projetos pessoais;
+
+ 4- Faça anotações dos pontos mais importantes;
+
+ 5- Dicas bônus: assista a aula e depois execute;
+
+ ";
+?>
