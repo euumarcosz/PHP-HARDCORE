caecf1244033a8705f9025dea7dca4f7db79dc31
euumarcosz <marcosdesigner98@gmail.com>
2024-06-11 17:20:54 -0300

    Introdução da Case Sensitive

---
+++ Conceitos basicos/01- CaseSensitive/IntroCaseSensi.php
@@ -0,0 +1,18 @@
+<?php
+
+ echo "
+
+ Case sensitivity
+ 
+ 1- Significado sensibilidade a casas maiúsculas e minúsculas;
+ 
+ 2- Para Instrução de PHP não temos essa diferença, ou seja: echo = ECHO;
+
+ 3- Porém para variáveis são case sensitive;
+
+ 4- Ou seja, $nome != $NOME;
+
+ 5- Obs: Veremos variáveis em detalhes mais adiante;
+
+ ";
+?>
