7a739bf26c6c10d94a728de2da1eaf5db4318b2b
euumarcosz <marcosdesigner98@gmail.com>
2024-06-11 17:11:49 -0300

    Estrutura de pastas

---
+++ Conceitos basicos/01- CaseSensitive/CaseSensitive.php
@@ -0,0 +1,15 @@
+<?php
+
+//Nao é case sensitive
+echo "the <br>";
+eChO "the 2 <br>";
+ECHO "the 3 <br>";
+
+//É case sensitive
+
+$nome = "The";
+$NOME = "Batman";
+
+echo $nome;
+echo "<br>";
+echo $NOME;
