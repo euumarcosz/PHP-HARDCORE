d60de64d817c5e0b37b3af55e2142c079f5bc0aa
euumarcosz <marcosdesigner98@gmail.com>
2024-06-12 15:57:33 -0300

    Espaços em branco

---
+++ Espaco_em_branco/espacosEmBranco.php
@@ -0,0 +1,16 @@
+<?php
+
+ echo "
+
+ Espaços em branco
+
+ 1- Para interpretação do código em PHP o espaço em branco é ignorado;
+ 
+ 2- Isso acontece pois o mesmo é removido antes da execução;
+
+ 3- A quebra de linha também é ignorada;
+
+ 4- Porém se utilizada de má forma pode gerar erros inesperados no código;
+
+ ";
+?>
