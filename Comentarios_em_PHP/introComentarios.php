2558ef658762fab1973456d5cb512a5339e415bd
euumarcosz <marcosdesigner98@gmail.com>
2024-06-12 16:24:08 -0300

    Comentarios

---
+++ Comentarios_em_PHP/introComentarios.php
@@ -0,0 +1,20 @@
+<?php
+
+ echo "
+
+ Comentários
+ 
+ 1- Os comentários servem para dar informações e direções importantes de como o código funciona;
+ 
+ 2- Iniciamos um comentário com //
+
+ 3- Todo conteúdo que está em um comentário é ignorado na execução;
+
+ 4- Não insira informações sensíveis nos comentários;
+
+ 5- Outra forma de inserir comentários é com #;
+
+ 6- Comentários muilt linhas são feitos com: /* comentário */ 
+
+ ";
+?>
