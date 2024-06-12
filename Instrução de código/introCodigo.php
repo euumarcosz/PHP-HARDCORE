186700b2979471c0b5fbf0a652444b0b9504baac
euumarcosz <marcosdesigner98@gmail.com>
2024-06-12 15:16:12 -0300

    Introdução de Código

---
+++ Instrução de código/introCodigo.php
@@ -0,0 +1,16 @@
+<?php
+
+ echo "
+
+ Instrução de código
+
+ 1- As instruções simples de PHP são sepadas por ponto e vírgula;
+ 
+ 2- Instrução simples são intruções de uma linha;
+
+ 3- Quando há uma instrução maior, como de condição ou repetição, a definição da mesma é dada por abertura e fechamento de chaves;
+
+ 4- Neste casos não precisaremos de ponto e vírgula;
+
+ ";
+?>
