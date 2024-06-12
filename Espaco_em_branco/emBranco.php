39479ab5609e29aa3ddbf920c47eeaf37a9c757b
euumarcosz <marcosdesigner98@gmail.com>
2024-06-12 16:15:25 -0300

    Testando espaço em branco

---
+++ Espaco_em_branco/emBranco.php
@@ -0,0 +1,22 @@
+<?php
+
+// Primeiro teste 
+echo                               "testando espaço em branco <br>";
+
+// Segundo teste
+echo "testando espaço em branco <br>";
+
+// Terceiro teste
+echo
+"quebra de linha <br>"
+;
+
+// Quarto teste
+echo "testando espaço em branco <br>"
+;
+
+// Quinto teste
+echo
+"quebra de
+linha <br>"
+;
