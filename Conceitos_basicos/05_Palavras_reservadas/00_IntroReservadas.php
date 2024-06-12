131d6626dc5edcafe97d5f02a8d7f787917837bb
euumarcosz <marcosdesigner98@gmail.com>
2024-06-12 17:47:20 -0300

    Prática

---
+++ Conceitos_basicos/05_Palavras_reservadas/00_IntroReservadas.php
@@ -0,0 +1,14 @@
+<?php
+
+ echo "
+
+ Palavras reservadas
+ 
+ 1- Algumas palavras são reservadas da linguagem e já tem suas funcionalidades definidas, então não podemos utilizar em nossos programas;
+ 
+ 2- Pois caso fosse possível poderíamos substituir a sua função original;
+
+ 3- Alguns exemplos são: echo, insteadof, else, interface, namespace, pow, __DIR__, __FILE__, endif, print, private protected, and require, public, as, break, case, for, finally, switch, throw e etc;
+
+ ";
+?>
