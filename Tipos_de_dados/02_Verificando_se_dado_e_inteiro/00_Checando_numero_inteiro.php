853b230ab19fb9368229091ddf0d74ffa63c8233
euumarcosz <marcosdesigner98@gmail.com>
2024-06-13 18:09:12 -0300

    Introdução

---
+++ Tipos_de_dados/02_Verificando_se_dado_e_inteiro/00_Checando_numero_inteiro.php
@@ -0,0 +1,16 @@
+<?php
+
+ echo "
+
+ Checando números inteiro
+ 
+ 1- Podemos validar se um dado é inteiro com a função is_int();
+ 
+ 2- Caso um número seja inteiro, será retornado true ( um outro tipo de dado);
+
+ 3- Caso não seja, receberemos um retorno de false ( tipo de dado tambén );
+
+ 4- Precisamos utilizar uma estrututa if para validar o valor;
+
+ ";
+?>
