1f630b3e8bcbd43597b6919ec84c758f9491fff2
euumarcosz <marcosdesigner98@gmail.com>
2024-08-07 20:02:55 -0300

    Checando float

--- Tipos_de_dados/03_Vericando_se_dado_e_float/03_Checando_float.php
+++ Tipos_de_dados/03_Vericando_se_dado_e_float/03_Checando_float.php
@@ -1,25 +1,79 @@
 <?php
+// Checando se um número é float
+echo "NÚMERO FLOAT <br>";
 
-// Checando números decimais
-// Float
+$valorFloat = 1.23; // Exemplo de número float
 
-
-$a = "Teste";
-$b = 12.8;
-
-if(is_float($a)) {
-    echo "Não é float (a)!: $a <br>"; // $a "False float"
-    echo "$a";
-    echo "<br>";
+if (is_float($valorFloat)) {
+    echo "É um float <br>";
+} else {
+    echo "Não é um float <br>";
 }
 
+echo "<br>";
 
-// Checando números decimais
+// Checando se um número não é float
+echo "NÃO É FLOAT <br>";
 
+$valorNaoFloat = "Não é um float"; // Exemplo de texto
 
-if(is_float($b)) {
-    echo "É float (b)!: $b <br>"; // $b "True float"
-    echo "<br>";
+if (!is_float($valorNaoFloat)) {
+    echo "Não é um float <br>";
 }
 
+echo "<br>";
 
+// Checando valores que não são float
+echo "NÃO É FLOAT <br>";
+
+$a = "Test";
+$b = "Test02";
+$c = "Test03";
+$d = "Test04";
+
+if (!is_float($a)) { // Verifica se não é um número float
+    echo "$a não é um float <br>";
+}
+
+if (!is_float($b)) {
+    echo "$b não é um float <br>";
+}
+
+if (!is_float($c)) {
+    echo "$c não é um float <br>";
+}
+
+if (!is_float($d)) {
+    echo "$d não é um float <br>";
+}
+
+echo "<br>";
+echo "Não é Float: $a, $b, $c, $d <br>";
+
+// Checando se números são float
+echo "É FLOAT <br>";
+
+$e = 1234.56;
+$f = 5678.90;
+$g = 9101.11;
+$h = 1213.14;
+
+if (is_float($e)) {
+    echo "$e é um float <br>";
+}
+
+if (is_float($f)) {
+    echo "$f é um float <br>";
+}
+
+if (is_float($g)) {
+    echo "$g é um float <br>";
+}
+
+if (is_float($h)) {
+    echo "$h é um float <br>";
+}
+
+echo "<br>";
+echo "São Floats: $e, $f, $g, $h <br>";
+?>
