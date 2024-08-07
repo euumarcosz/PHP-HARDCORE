e4a23352bfee6cceef5cb2038d37ddf8fd70b5c3
euumarcosz <marcosdesigner98@gmail.com>
2024-08-07 18:35:21 -0300

    Indotrução de números decimais

---
+++ Tipos_de_dados/03_Vericando_se_dado_e_float/00_Checando_numero_decimais.php
@@ -0,0 +1,14 @@
+<?php
+
+ echo "
+
+ Checando números decimais
+ 
+ 1- Os floats são todos os números com casas decimais;
+ 
+ 2- Como o padrão universal é da língua inglesa, temos a separação de casas ( com . e não , );
+
+ 3- Exemplos de floats: ( 2.123, 0.04, -12.8);
+
+";
+?>
