c4a2d192029e3a94ff4127f6739b4d06e3837a72
Marcos Vinicius <107082481+euumarcosz@users.noreply.github.com>
2024-08-07 18:47:49 -0300

    Introdução de Números com casas decimais

--- Tipos_de_dados/03_Vericando_se_dado_e_float/00_Checando_numero_decimais.php
+++ Tipos_de_dados/03_Vericando_se_dado_e_float/00_Checando_numero_decimais.php
@@ -8,7 +8,7 @@
  
  2- Como o padrão universal é da língua inglesa, temos a separação de casas ( com . e não , );
 
- 3- Exemplos de floats: ( 2.123, 0.04, -12.8);
+ 3- Exemplos de floats: ( 2.123, 0.04, -12.8 );
 
 ";
 ?>
