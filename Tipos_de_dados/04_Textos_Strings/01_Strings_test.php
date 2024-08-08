47642288b332d8d0369701ebedb4e17b0e3aa046
euumarcosz <marcosdesigner98@gmail.com>
2024-08-08 18:12:34 -0300

    Strings

--- Tipos_de_dados/04_Textos_Strings/01_Strings_test.php
+++ Tipos_de_dados/04_Textos_Strings/01_Strings_test.php
@@ -1,6 +1,9 @@
 <?php
 
 echo "Testando texto de aspas duplas <br>";
+
     echo 'Testando texto de aspas simples <br>';
+
         echo "Ele disse: 'Olá!' <br>";
+        
             echo 'Ele dosse: "Olá!" <br>';
