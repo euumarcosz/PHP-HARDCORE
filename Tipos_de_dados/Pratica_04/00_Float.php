e224f289d9c954cfde049c4cceef1173038241af
euumarcosz <marcosdesigner98@gmail.com>
2024-08-08 16:18:55 -0300

    Pratica_04

---
+++ Tipos_de_dados/Pratica_04/00_Float.php
@@ -0,0 +1,23 @@
+<?php
+
+echo 4.15;
+echo "<br>";
+echo 12.12;
+echo "<br>";
+
+$a = -78.1; // Número float negativo 
+
+echo $a;
+echo "<br>";
+
+// Defina a variavél $valorFloat 
+$valorFloat = $a;
+
+if(is_float($valorFloat)) {
+    echo "Sim, podemos ter floats negativos!";
+}
+echo "<br>";
+
+if(is_float($valorFloat)) {
+    echo "É um inteiro";
+} 
