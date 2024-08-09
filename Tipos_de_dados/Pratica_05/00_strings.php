09e990500b35a98044b4510a47a408545978e270
euumarcosz <marcosdesigner98@gmail.com>
2024-08-09 19:46:08 -0300

    Pratica_05

--- Tipos_de_dados/Pratica_05/00_strings.php
+++ Tipos_de_dados/Pratica_05/00_strings.php
@@ -1,9 +1,14 @@
 <?php
-
+// Exibindo texto com aspas simples
 echo 'Testando aspas simples <br>';
+
+// Exibindo texto com aspas duplas
 echo "Testando aspas duplas <br>";
 
-$nome = "Batman";
-$idade = 25;
+// Inicializando variáveis
+$nome = "Batman";  // Nome de uma pessoa
+$idade = 25;       // Idade da pessoa
 
+// Usando aspas duplas para permitir a interpolação de variáveis
 echo "Olá, eu sou o $nome! Você tem $idade anos. <br>";
+?>
