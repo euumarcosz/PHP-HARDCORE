1b1fa027ebe8b801f775668d33f14e54bb1bc9df
euumarcosz <marcosdesigner98@gmail.com>
2024-08-09 19:26:19 -0300

    Checagem de string

---
+++ Tipos_de_dados/05_Verificando_se_dado_e_string/01_Checagem_de_Strings.php
@@ -0,0 +1,16 @@
+<?php
+
+$str = "Batman";
+$num = 25;
+
+if(is_string($str)) {
+    echo "$str é uma string 1<br>";
+}
+
+if(is_string($num)) {
+    echo "$num é uma string 2<br>";
+}
+
+if(is_string($str)) {
+    echo "É uma string 3<br>";
+}
