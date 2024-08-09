4f5953337870f9c29dd40dddcd029ff35937f853
euumarcosz <marcosdesigner98@gmail.com>
2024-08-09 19:17:37 -0300

    Introdução

--- Tipos_de_dados/05_Verificando_se_dado_e_string/00_Checando_string.php
+++ Tipos_de_dados/05_Verificando_se_dado_e_string/00_Checando_string.php
@@ -1,7 +1,9 @@
 <?php
 
  echo "
+
  Checando se é string 
+ 
  1- Podemos utilizar a função [ is_string() ] para verificar se um dado é uma string;
  2- A função recebe um [ valor como parâmetro ];
  3- Novamente, receberemos [ true ou false ], dependendo do dado enviado;
